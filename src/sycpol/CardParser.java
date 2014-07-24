544f80a4c2e397eb6db68812f88850fca8c74df2
Fergusq <fergusq@kaivos.org>
2014-07-24 18:57:57 +0300

    character set

--- src/sycpol/CardParser.java
+++ src/sycpol/CardParser.java
@@ -4,6 +4,8 @@ import sycpol.Sycpol;
 import java.util.List;
 
 public class CardParser {
+    public static final String STANDARD_CHARACTER_SET = "^[A-Z0-9\\Q.:,;!?-=\"'$%&|<>()/*^+ \\E]*$";
+
     private String str;
 
     CardParser() {
@@ -11,10 +13,10 @@ public class CardParser {
     }
     
     public void loadCard(List<String> lines) {
-	loadCard(lines, 22, 22, "STANDARD SYSTEM CARD I");
+	loadCard(lines, 22, 22, STANDARD_CHARACTER_SET, "STANDARD SYSTEM CARD I");
     }
 
-    public void loadCard(List<String> lines, int width, int height, String model) {
+    public void loadCard(List<String> lines, int width, int height, String charset, String model) {
 	
 	for (String line : lines) {
 	    if (line.length() > width)
@@ -25,21 +27,21 @@ public class CardParser {
 	if (lines.size() > height)
 	    Sycpol.exit("SYNTAX ERROR: INCOMPATIBLE CARD HEIGHT.");
 	
-	loadTape(lines, width, "CARD", model);
+	loadTape(lines, width, charset, "CARD", model);
     }
 
     public void loadTape(List<String> lines) {
-	loadTape(lines, 22, "TAPE", "STANDARD SYSTEM TAPE I");
+	loadTape(lines, 22, STANDARD_CHARACTER_SET, "TAPE", "STANDARD SYSTEM TAPE I");
     }
     
-    public void loadTape(List<String> lines, int width, String type, String model) {
+    public void loadTape(List<String> lines, int width, String charset, String type, String model) {
 
 	if (lines.size() < 2)
 	    Sycpol.exit("SYNTAX ERROR: EMPTY CARD");
 	
 	for (String line : lines) {
-	    if (line.matches("[a-z]"))
-		Sycpol.exit("SYNTAX ERROR: UNSUPPORTED CHARACTER SET.");
+	    if (!line.matches(charset))
+		Sycpol.exit("SYNTAX ERROR: UNSUPPORTED CHARACTER SET.", "LINE:\n"+line);
 	}
 	
 	// Check card declaration (PROGRAM CARD or COMMENT CARD)
